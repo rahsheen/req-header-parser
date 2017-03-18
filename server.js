7fb298855990a058b6ad2c5a7b63261b5341241c
R. Anthony Porter <rah@sheenonline.biz>
2017-03-18 10:34:41 -0400

    Cleaned up ua-parser usage. Challenge complete.

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express")
-const moment = require('moment')
 const app = express();
+const p = require('ua-parser');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.status(200).send(req.header('user-agent'))
-    })
+    const lang = req.headers["accept-language"].split(',')[0]+"";
+    const userAgent = req.headers['user-agent'];
 
-app.listen(port, () => {
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     ip = ip.split(',')[0];
+     ip = ip.split(':').slice(-1)[0];
+
+    res.status(200).send({
+        "IP": ip,
+        "lang":  lang,
+        "OS": p.parseOS(userAgent).toString()
+    })
+})
+.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
